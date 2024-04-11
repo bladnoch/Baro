98d356e7a574ee95fa744ce6725a4a9e616e4928
DK <dongwook5478@gmail.com>
2024-04-12 00:47:07 +0900

    PersonalInfo,PersonalInfoImpl.java

---
+++ src/main/java/firstmarket/koreashop/profile/PersonalInfo.java
@@ -0,0 +1,8 @@
+package firstmarket.koreashop.profile;
+
+public interface PersonalInfo {
+    String[] MemberUploadedItem;
+    String[] MemberReservedItem;
+    String[] likedItem;
+    void deleteMember;
+}
---
+++ src/main/java/firstmarket/koreashop/profile/PersonalInfoImpl.java
@@ -0,0 +1,5 @@
+package firstmarket.koreashop.profile;
+
+public class PersonalInfoImpl {
+
+}
