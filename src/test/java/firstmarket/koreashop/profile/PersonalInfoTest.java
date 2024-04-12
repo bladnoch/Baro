0f39faad6b6211dee409a94ae783a8cc54d90ad6
DK <dongwook5478@gmail.com>
2024-04-13 03:05:17 +0900

    add PersonalInfoTest.java

---
+++ src/test/java/firstmarket/koreashop/profile/PersonalInfoTest.java
@@ -0,0 +1,14 @@
+package firstmarket.koreashop.profile;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonalInfoTest {
+
+    @Test
+    void displayElementsOfProfileTest() {
+
+    }
+
+}
