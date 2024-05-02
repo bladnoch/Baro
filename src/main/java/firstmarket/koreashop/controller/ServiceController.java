f0a1069538cd7d8b73b90721b76a7aa07eadc847
DK <dongwook5478@gmail.com>
2024-05-02 14:19:13 +0900

    add saveMember api

--- src/main/java/firstmarket/koreashop/controller/ServiceController.java
+++ src/main/java/firstmarket/koreashop/controller/ServiceController.java
@@ -2,23 +2,28 @@ package firstmarket.koreashop.controller;
 
 import firstmarket.koreashop.dto.MemberServiceRequest;
 import firstmarket.koreashop.member.Member;
+import firstmarket.koreashop.member.MemberRepo;
 import firstmarket.koreashop.member.MemberService;
-import firstmarket.koreashop.member.MemberServiceImpl;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ServiceController {
 
+    @Autowired
+    MemberService memberService;
+
     /**
-     * find account Id by saved phone number
-     * @param phoneNumber : String phoneNumber
-     * @return get String MemberId
+     * 멤버 저장 기능
+     * 들어온 멤버 정보를 저장한다. 반환은 하지 않는다.
+     * 추후 클라이언트에서 저장여부 확인을 위해 return 추가할 수 있음
+     * 추가하지 못했을 때 문제를 return
      */
-//    String findMemberIdByPhoneNumber(String phoneNumber); //get
-    @GetMapping("/findId")
-    public String getMemberIdByPhoneNumber(MemberServiceRequest phoneNumber){
-        return phoneNumber.getMemberId();
+    @GetMapping("/member")
+    public void saveMember(@RequestBody Member member){
+        memberService.join(member);
     }
+
+
 }
--- src/main/java/firstmarket/koreashop/dto/MemberServiceRequest.java
+++ src/main/java/firstmarket/koreashop/dto/MemberServiceRequest.java
@@ -1,24 +1,18 @@
 package firstmarket.koreashop.dto;
 
+
 import firstmarket.koreashop.member.Member;
 import firstmarket.koreashop.member.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 
 public class MemberServiceRequest {
+
     private final String phoneNumber;
 
     public MemberServiceRequest(String phoneNumber) {
         this.phoneNumber = phoneNumber;
-    }
-
-
-
-    public String getMemberId() {
-        if(phoneNumber.equals("01046054434")){
-            return "dong5478";
-        }
-        else
-            return "not a member";
-
     }
 
 }
